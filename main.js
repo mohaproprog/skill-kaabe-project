2690ea816bb2f92a71ed466652e56e68a2d02cb7
mohaproprog <mohaproprog@gmail.com>
2025-08-18 02:03:02 +0100

    added the service section

--- main.js
+++ main.js
@@ -14,4 +14,37 @@ hide.addEventListener("click",()=>{
     listNav.style.transition = "0.5s";
     joinUs.style.transition = "0.5s";
 
+});
+
+
+// ------------------------- see more--------------
+
+const seeMore = document.querySelectorAll(".see-more");
+
+seeMore.forEach(btnsee =>{
+    btnsee.addEventListener("click",()=>{
+         const box = btnsee.parentElement;
+         const moreText = box.querySelector(".seeMore");
+         if(moreText.style.display === "inline"){
+            btnsee.textContent = "see more";
+            moreText.style.display = "none";
+         }
+         else{
+            
+            moreText.style.display = "inline";
+            btnsee.textContent = "see less";
+            
+         }
+
+        //  moreText.forEach(moreTextTarget=>{
+        //     moreTextTarget.style.display = "inline";
+        //     if (moreTextTarget.target.style.display != "inline"){
+        //         moreTextTarget.parentElement.style.maxPadding = "100px";
+        //     }
+        //  })
+
+
+    })
+   
+
 })
